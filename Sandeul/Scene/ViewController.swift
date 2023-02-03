dd18de3288eda94aa1e500fae963637f27508696
annie <knr9144@gmail.com>
2023-02-03 19:49:20 +0900

    Initial Commit

---
+++ Sandeul/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sandeul
+//
+//  Created by 나리강 on 2023/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
